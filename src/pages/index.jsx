827e1152301e499ec9ba7b1d9f7382afca4d0eac
PoncoAdi-prog <Poncoadinugroho20@gmail.com>
2023-03-18 09:55:22 +0700

    first commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// import { Navigate } from 'react-router-dom';
+import AppLayout from '../components/layouts/app.layout';
+
+
+const MainPage = () => (
+    <AppLayout>
+        <h1> Helo Ponco World </h1>
+    </AppLayout>
+);
+
+
+export default MainPage
